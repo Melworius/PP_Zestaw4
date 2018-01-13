bbdf021a75e360c208b1804ffdc3c0c89ebb952c
PAWEŁ <PAWE@192.168.1.71>
2018-01-13 13:05:21 +0100

    a

---
+++ PP_Zestaw4/src/Zad6.java
@@ -0,0 +1,22 @@
+/* Napisz program, który umo¿liwia szyfrowanie podanego ci¹gu znaków przy u¿yciu
+ * szyfru Cezara, który jest szczególnym przypadkiem szyfru podstawieniowego
+ * monoalfabetycznego.
+ * U¿ytkownik program podaje tekst do zaszyfrowania oraz liczbê n, o któr¹ przesuniêty
+ * jest alfabet za pomoc¹ którego szyfrujemy tekst. Dla uproszczenia mo¿na
+ * przyj¹æ, ¿e ³añuch wejœciowy sk³ada siê tylko z ma³ych liter alfabetu angielskiego,
+ * tj. ’a’ – ’z’ (26 znaków) oraz spacji.
+ * Przyk³ad 1.
+ * Podaj ³añcuch znaków do zaszyfrowania: abrakadabraz
+ * Podaj przesuniêcie: 2
+ * Zaszyfrowany tekst: cdtcmcfcdtcb
+ * Przyk³ad 2.
+ * Podaj ³añcuch znaków do zaszyfrowania: cdtcmcfcdtcb
+ * Podaj przesuniêcie: -2
+ * Zaszyfrowany tekst: abrakadabraz
+ */
+import java.util.*;
+public class Zad6 {
+	public static void main(String[] args) {
+		
+		}
+}
